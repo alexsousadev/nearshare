a8df8b89a1f822ab50eac6d47a7fcfc3242a5616
alexsousadev <alexandrosousapro@gmail.com>
2026-07-11 21:38:44 -0300

    refactor: separate connection and room management

--- server/services/device.ts
+++ server/services/device.ts
@@ -2,15 +2,16 @@ import { IncomingMessage } from "http";
 import { WebSocket } from "ws";
 
 class Device {
-
     request: IncomingMessage
     connection: WebSocket
     id: string
+    name: string
 
     constructor(req: IncomingMessage, sock: WebSocket){
         this.request = req
         this.connection = sock
         this.id = Math.random().toString(36).substr(2, 9)
+        this.name = this.getName()
     }
 
     getIP(): string {
@@ -19,6 +20,10 @@ class Device {
             ? forwarded.split(',')[0].trim() 
             : this.request.socket.remoteAddress;
         return ip || 'unknown';
+    }
+
+    getName(): string {
+        return this.id.substring(0, 4)
     }
     
 }
